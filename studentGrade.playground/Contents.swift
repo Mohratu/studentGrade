ff16c70f215effd787cf8c4b5b22782a6bb3d465
Mohratu <Mohraturki.23@gmail.com>
2024-08-06 14:08:04 +0300

    Initial Commit

---
+++ studentGrade.playground/Contents.swift
@@ -0,0 +1,21 @@
+let studentGrade = 80
+
+switch studentGrade {
+
+case 90:
+    print("Excellent")
+    
+case 80:
+    print("Vary Good ")
+    
+case 70:
+    print("Good")
+    
+case 60:
+    print("Pass")
+    
+default:
+    print("beiow")
+}
+
+
